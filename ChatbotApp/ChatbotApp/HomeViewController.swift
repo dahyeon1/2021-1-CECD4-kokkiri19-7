4de3980ba111f4f2decc13d716f92898912d0819
yeon <jryoun0404@gmail.com>
2021-05-28 12:10:19 +0900

    feat: Setup homeview using tab bar and setup main page with several images from Junggu

---
+++ ChatbotApp/ChatbotApp/HomeViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  ChatbotApp
+//
+//  Created by Yeon on 2021/05/10.
+//
+
+import UIKit
+
+var images = ["메인화면 소식1", "메인화면 소식2", "메인화면 소식3", "메인화면 소식4"]
+
+class HomeViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var pageControl: UIPageControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupPageControl()
+        setupSwipeGesture()
+    }
+    
+    private func setupPageControl() {
+        pageControl.numberOfPages = images.count
+        pageControl.currentPage = 0
+        
+        pageControl.pageIndicatorTintColor = UIColor.lightGray
+        pageControl.currentPageIndicatorTintColor = UIColor.black
+        imageView.image = UIImage(named: images[0])
+    }
+    
+    private func setupSwipeGesture() {
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(HomeViewController.respondToSwipeGesture(_:)))
+        swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(HomeViewController.respondToSwipeGesture(_:)))
+        swipeRight.direction = UISwipeGestureRecognizer.Direction.right
+        self.view.addGestureRecognizer(swipeRight)
+    }
+    
+    /// Check gesture whether direction is right or left
+    /// Right: next page
+    /// Left: before page
+    /// - Parameter gesture: user's gesture
+    @objc func respondToSwipeGesture(_ gesture: UIGestureRecognizer) {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizer.Direction.left:
+                pageControl.currentPage -= 1
+            case UISwipeGestureRecognizer.Direction.right:
+                pageControl.currentPage += 1
+            default:
+                break
+            }
+            imageView.image = UIImage(named: images[pageControl.currentPage])
+        }
+    }
+    
+    @IBAction func pageChanged(_ sender: UIPageControl) {
+        imageView.image = UIImage(named: images[pageControl.currentPage])
+    }
+}
+
