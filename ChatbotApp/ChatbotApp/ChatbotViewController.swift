e63ff52d6117b2a7635c2c79adf0d368c88d1511
yeon <jryoun0404@gmail.com>
2021-05-31 01:42:50 +0900

    feat: Add toolbar to Chatbot view

--- ChatbotApp/ChatbotApp/ChatbotViewController.swift
+++ ChatbotApp/ChatbotApp/ChatbotViewController.swift
@@ -11,7 +11,7 @@ import Toolbar
 import SnapKit
 
 class ChatbotViewController: UIViewController, UITextViewDelegate {
-
+    
     //MARK: Properties
     // tool bar
     let containerView = UIView()
@@ -43,6 +43,7 @@ class ChatbotViewController: UIViewController, UITextViewDelegate {
         
         configureAutoLayout()
         setupChatbotViewControllerBackground()
+        addToolBar()
     }
     
     private func configureAutoLayout() {
@@ -61,6 +62,21 @@ class ChatbotViewController: UIViewController, UITextViewDelegate {
         gradientLayer.locations = [0.0, 1.0]
         gradientLayer.frame = self.view.bounds
         containerView.layer.addSublayer(gradientLayer)
+    }
+    
+    private func addToolBar() {
+        containerView.addSubview(toolbar)
+        self.toolbarBottomConstraint = self.toolbar.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: 0)
+        self.toolbarBottomConstraint?.isActive = true
+        let bottomView = UIView()
+        bottomView.backgroundColor = .chatBackgroundEnd
+        containerView.addSubview(bottomView)
+        bottomView.snp.makeConstraints { (make) -> Void in
+            make.right.equalTo(containerView)
+            make.left.equalTo(containerView)
+            make.top.equalTo(toolbar.snp.bottom)
+            make.height.equalTo(100)
+        }
     }
     
     override func viewDidLoad() {
