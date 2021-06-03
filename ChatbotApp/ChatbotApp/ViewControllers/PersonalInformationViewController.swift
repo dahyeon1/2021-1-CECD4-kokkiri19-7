3a3f72eba605a14a5f6f58e6bd148dae22abe5a4
yeon <jryoun0404@gmail.com>
2021-06-03 23:41:00 +0900

    [#3] feat: Register user to kommunicate

--- ChatbotApp/ChatbotApp/ViewControllers/PersonalInformationViewController.swift
+++ ChatbotApp/ChatbotApp/ViewControllers/PersonalInformationViewController.swift
@@ -6,8 +6,12 @@
 //
 
 import UIKit
+import Kommunicate
 
 final class PersonalInformationViewController: UIViewController {
+    
+    let userID = Kommunicate.randomId()
+    let kmUser = KMUser()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,5 +20,18 @@ final class PersonalInformationViewController: UIViewController {
     
     private func setupPersonalInformationViewControllerBackground() {
         self.view.backgroundColor = UIColor.chatBackgroundEnd
+    }
+    
+    private func registerUser() {
+        kmUser.userId = userID
+        kmUser.displayName = "사용자"
+        kmUser.applicationId = Configuration.AppID
+        
+        Kommunicate.registerUser(kmUser) { response, error in
+            guard error == nil else {
+                return
+            }
+            print("로그인에 성공하였습니다.")
+        }
     }
 }
