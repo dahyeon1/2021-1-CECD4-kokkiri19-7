fe69940786811bff82d384606a7870a8f6a0539f
yeon <jryoun0404@gmail.com>
2021-06-03 16:38:32 +0900

    [#3] feat: Create configuration file

---
+++ ChatbotApp/ChatbotApp/Network/Configuration.swift
@@ -0,0 +1,13 @@
+//
+//  Configure.swift
+//  ChatbotApp
+//
+//  Created by Yeon on 2021/06/01.
+//
+
+import Foundation
+
+enum Configuration {
+    static let AppID = "27c785abede1dd83afc76732df5e2524c"
+    static let APIKey = "Kg5OXbFgZstAuwTEhdbvRi1O8UsOAfcm"
+}
