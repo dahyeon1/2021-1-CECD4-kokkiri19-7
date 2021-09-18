cf22c9ce2fc19fd9b1e1caa7693c3fdc382bf7a8
yeon <jryoun0404@gmail.com>
2021-09-18 16:43:03 +0900

    feat: Add identifier property

--- ChatbotApp/ChatbotApp/ViewControllers/PersonalInformationViewController.swift
+++ ChatbotApp/ChatbotApp/ViewControllers/PersonalInformationViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 final class PersonalInformationViewController: UIViewController {
-    
+    static let identifier = "PersonalInformationViewController"
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
