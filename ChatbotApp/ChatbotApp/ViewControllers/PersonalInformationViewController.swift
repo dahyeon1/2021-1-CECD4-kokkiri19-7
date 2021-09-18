8adccc93719595593d2c60e0d31cecf91df1a869
yeon <jryoun0404@gmail.com>
2021-09-18 16:43:28 +0900

    feat: Add IBOutlet properties for settingView

--- ChatbotApp/ChatbotApp/ViewControllers/PersonalInformationViewController.swift
+++ ChatbotApp/ChatbotApp/ViewControllers/PersonalInformationViewController.swift
@@ -9,7 +9,18 @@ import UIKit
 
 final class PersonalInformationViewController: UIViewController {
     static let identifier = "PersonalInformationViewController"
-
+    //MARK:- properties for settingView
+    @IBOutlet weak var settingScrollView: UIScrollView!
+    @IBOutlet weak var settingBarButton: UIBarButtonItem!
+    @IBOutlet weak var nicknameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var sexSegmentControl: UISegmentedControl!
+    @IBOutlet weak var ageDatePicker: UIDatePicker!
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var provinceTextField: UITextField!
+    @IBOutlet weak var errorMessageTextLabel: UILabel!
+    @IBOutlet weak var doneButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
