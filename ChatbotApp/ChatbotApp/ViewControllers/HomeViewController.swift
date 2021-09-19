d45380e088e0d5af6752917b80028e3605937910
yeon <jryoun0404@gmail.com>
2021-09-20 00:35:34 +0900

    feat: Add IBOutlet properties

--- ChatbotApp/ChatbotApp/ViewControllers/HomeViewController.swift
+++ ChatbotApp/ChatbotApp/ViewControllers/HomeViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 var images = ["메인화면소식1", "메인화면소식2", "메인화면소식3", "메인화면소식4"]
 
 final class HomeViewController: UIViewController {
+    @IBOutlet private weak var nicknameLabel: UILabel!
+    @IBOutlet private weak var locationLabel: UILabel!
+    @IBOutlet private weak var collectionView: UICollectionView!
+    private var pageControl: UIPageControl = {
+        let pageControl = UIPageControl()
+        pageControl.translatesAutoresizingMaskIntoConstraints = false
+        pageControl.hidesForSinglePage = true
+        pageControl.pageIndicatorTintColor = UIColor.lightGray
+        pageControl.currentPageIndicatorTintColor = UIColor.darkGray
+        return pageControl
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
