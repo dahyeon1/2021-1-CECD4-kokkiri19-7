7e42416616cf96f6083c49878112c10f846789fc
yeon <jryoun0404@gmail.com>
2021-09-20 00:26:16 +0900

    Create BannerImageCollectionViewCell file

---
+++ ChatbotApp/ChatbotApp/ViewModel/View/BannerImageCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  BannerImageCollectionViewCell.swift
+//  ChatbotApp
+//
+//  Created by Yeon on 2021/09/19.
+//
+
+import UIKit
+
+class BannerImageCollectionViewCell: UICollectionViewCell {
+    
+}
