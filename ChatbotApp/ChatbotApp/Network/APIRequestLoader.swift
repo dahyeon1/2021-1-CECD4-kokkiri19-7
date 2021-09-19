64294810c3c1221583a93a3278e382b3c7a21d4a
yeon <jryoun0404@gmail.com>
2021-09-20 01:07:45 +0900

    feat: Add APIRequest protocol

--- ChatbotApp/ChatbotApp/Network/APIRequestLoader.swift
+++ ChatbotApp/ChatbotApp/Network/APIRequestLoader.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+protocol APIRequest {
+    associatedtype RequestDataType
+    associatedtype ResponseDataType
+    
+    func makeRequest(from data: RequestDataType) throws -> URLRequest
+    func parseResponse(data: Data) throws -> ResponseDataType
+}
